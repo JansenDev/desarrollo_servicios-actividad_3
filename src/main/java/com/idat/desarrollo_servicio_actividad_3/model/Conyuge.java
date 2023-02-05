39abba7cfafdc05d7173e26c634e464249f4f295
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2023-02-04 19:30:02 -0500

    tablas

---
+++ src/main/java/com/idat/desarrollo_servicio_actividad_3/model/Conyuge.java
@@ -0,0 +1,60 @@
+package com.idat.desarrollo_servicio_actividad_3.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Conyuge implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int conyuge_id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private int edad;
+
+    public Conyuge(int conyuge_id, String nombre, String apellidos, int edad) {
+        this.conyuge_id = conyuge_id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public Conyuge() {
+        
+    }
+
+    public int getConyuge_id() {
+        return conyuge_id;
+    }
+
+    public void setConyuge_id(int conyuge_id) {
+        this.conyuge_id = conyuge_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ src/main/java/com/idat/desarrollo_servicio_actividad_3/model/Profesor.java
@@ -0,0 +1,72 @@
+package com.idat.desarrollo_servicio_actividad_3.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="profesores")
+public class Profesor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int profesor_id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private int edad;
+
+    private String email;
+
+    public Profesor(int profesor_id, String nombre, String apellidos, int edad, String email) {
+        this.profesor_id = profesor_id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.email = email;
+    }
+
+    public Profesor() {
+
+    }
+
+    public int getProfesor_id() {
+        return profesor_id;
+    }
+
+    public void setProfesor_id(int profesor_id) {
+        this.profesor_id = profesor_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
