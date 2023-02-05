be7e6bad7c1bcd53f55e6eeb7ab4c9fae8a972e1
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2023-02-04 19:04:15 -0500

    init project

---
+++ src/main/java/com/idat/desarrollo_servicio_actividad_3/DesarrolloServicioActividad3Application.java
@@ -0,0 +1,13 @@
+package com.idat.desarrollo_servicio_actividad_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloServicioActividad3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesarrolloServicioActividad3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/idat/desarrollo_servicio_actividad_3/DesarrolloServicioActividad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.desarrollo_servicio_actividad_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloServicioActividad3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
